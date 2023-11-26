7ec53e65ba0b33b74c6c03bb0cfbb1736780a2fe
ftodoric <fptodoric@gmail.com>
2023-11-27 00:31:45 +0100

    Add mwss

--- src/lib/game/cgl-patterns.ts
+++ src/lib/game/cgl-patterns.ts
@@ -5,6 +5,11 @@ import type { BoardMatrix } from "./types";
  * When loaded into the game, they're centered.
  */
 
+/**
+ * Spaceships
+ *
+ * They represent moving patterns.
+ */
 export const glider: BoardMatrix = [
   [true, false, false],
   [false, true, true],
@@ -16,4 +21,12 @@ export const lwss: BoardMatrix = [
   [true, false, false, false, false],
   [true, false, false, false, true],
   [true, true, true, true, false],
+];
+
+export const mwss: BoardMatrix = [
+  [false, false, false, true, false, false],
+  [false, true, false, false, false, true],
+  [true, false, false, false, false, false],
+  [true, false, false, false, false, true],
+  [true, true, true, true, true, false],
 ];

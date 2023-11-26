9365ad78a6164f11bcba8361ee5d5a491a2ed8dc
ftodoric <fptodoric@gmail.com>
2023-11-27 00:35:29 +0100

    Add hwss

--- src/lib/game/cgl-patterns.ts
+++ src/lib/game/cgl-patterns.ts
@@ -38,3 +38,12 @@ export const mwss: BoardMatrix = [
   [true, false, false, false, false, true],
   [true, true, true, true, true, false],
 ];
+
+// Heavyweight Spaceship
+export const hwss: BoardMatrix = [
+  [false, false, false, true, true, false, false],
+  [false, true, false, false, false, false, true],
+  [true, false, false, false, false, false, false],
+  [true, false, false, false, false, false, true],
+  [true, true, true, true, true, true, false],
+];

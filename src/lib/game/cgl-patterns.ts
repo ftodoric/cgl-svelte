d61a7069455974b99a2692d9375dd554eb28ca86
ftodoric <fptodoric@gmail.com>
2023-11-27 00:25:40 +0100

    Add lwss pattern

--- src/lib/game/cgl-patterns.ts
+++ src/lib/game/cgl-patterns.ts
@@ -8,3 +8,10 @@ export const glider: BoardMatrix = [
   [false, false, false, false, false, false, false],
   [false, false, false, false, false, false, false],
 ];
+
+export const lwss: BoardMatrix = [
+  [false, true, false, false, true],
+  [true, false, false, false, false],
+  [true, false, false, false, true],
+  [true, true, true, true, false],
+];

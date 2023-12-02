24f9e0c3ce96c1b497bbbc7ffba2291efda463b4
ftodoric <fptodoric@gmail.com>
2023-12-02 01:40:51 +0100

    Add RandyGG font face

---
+++ src/modules/DrawerMenu/PatternButton/PatternButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "./styles.css";
+
+  import type { BoardMatrix } from "$lib/game/types";
+  import Board from "../../Board/Board.svelte";
+
+  export let pattern: { name: string; matrix: BoardMatrix };
+</script>
+
+<button class="pattern-button" on:click>
+  <div
+    style="display: flex; justify-content: space-between; align-items: center;"
+  >
+    <div style="color: #fff;">{pattern.name}</div>
+
+    <Board boardMatrix={pattern.matrix} readOnly />
+  </div>
+</button>
